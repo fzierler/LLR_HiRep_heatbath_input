d661cc24cd05c1d53568ed3e88b719248e55d70b
Fabian Zierler <fabian.zierler@a1.net>
2025-03-14 18:52:31 +0000

    make creation python script more compact

--- setup_run.py
+++ setup_run.py
@@ -3,7 +3,7 @@
 
 import llranalysis.inputfiles as ifiles
 import pandas as pd 
-import os
+import os.path as op
 
 outdir    = "./output/"
 input_dir = "./input/templates/"
@@ -30,27 +30,21 @@ Lt  = input_data["Lt"].values[0]
 Ls  = input_data["Ls"].values[0]
 Rep = input_data["n_replicas"].values[0]
 run_name = "LLR_"+str(Lt)+"x"+str(Ls)+"_"+str(Rep)
-folder = os.path.join(outdir,run_name)
+folder = op.join(outdir,run_name)
 
 ifiles.predat_from_csv(folder,infofile)
 ifiles.copy_identical_files(folder,input_dir)
-
-infile  = os.path.join(input_dir,"base","input_file")
-outfile = os.path.join(folder,"base","input_file")
-ifiles.input_files_from_csv(infile,outfile,infofile)
-
-infile  = os.path.join(input_dir,"base","input_file_rep")
-outfile = os.path.join(folder,"base","input_file_rep")
-ifiles.input_files_from_csv(infile,outfile,infofile)
+ifiles.input_files_from_csv(op.join(input_dir,"base","input_file"),     op.join(folder,"base","input_file"),infofile)
+ifiles.input_files_from_csv(op.join(input_dir,"base","input_file_rep"), op.join(folder,"base","input_file_rep"),infofile)
 
 if template_dir == "local_reproducible":
-    infile  = os.path.join(input_dir,"setup_llr_repeat_local.sh")
+    infile  = op.join(input_dir,"setup_llr_repeat_local.sh")
 else:
-    infile  = os.path.join(input_dir,"setup_llr_repeat.sh")
-outfile = os.path.join(folder,"setup_llr_repeat.sh")
+    infile  = op.join(input_dir,"setup_llr_repeat.sh")
+outfile = op.join(folder,"setup_llr_repeat.sh")
 ifiles.input_files_from_csv(infile,outfile,infofile)
 
 for name in bash_files:
-    infile  = os.path.join(input_dir,template_dir,name)
-    outfile = os.path.join(folder,name)
+    infile  = op.join(input_dir,template_dir,name)
+    outfile = op.join(folder,name)
     ifiles.edit_batch_files(infile,outfile,infofile,cores_per_node)
