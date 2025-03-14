476383ddaeaf8298a39c907a4d37c25ac0c30e59
Fabian Zierler <fabian.zierler@a1.net>
2025-03-14 16:18:54 +0000

    replace remaining echo commands by sed-c

--- input/templates/setup_replicas.sh
+++ input/templates/setup_replicas.sh
@@ -53,10 +53,10 @@ do
     de=${stringarray[2]}
     mkdir Rep_${i}
     cp $INPUTFILE Rep_${i}/input_file
-    echo "rlx_seed = ${RANDOM}" >> Rep_${i}/input_file
-    echo "llr:S0 = $E" >> Rep_${i}/input_file
-    echo "llr:dS = ${de}" >> Rep_${i}/input_file
-    echo "llr:starta = ${A}" >> Rep_${i}/input_file
+    sed -i "/rlx_seed/c\rlx_seed = ${RANDOM}" Rep_${i}/input_file
+    sed -i "/llr:S0/c\llr:S0 = $E"            Rep_${i}/input_file
+    sed -i "/llr:dS/c\llr:dS = ${de}"         Rep_${i}/input_file
+    sed -i "/llr:starta/c\llr:starta = ${A}"  Rep_${i}/input_file
     sed -i "/llr:Smin =/c\llr:Smin = ${Emin}" Rep_${i}/input_file
     sed -i "/llr:Smax =/c\llr:Smax = ${Emax}" Rep_${i}/input_file
 
