c39daa8cf56f085689dc1f191e372ad6a62dc9bd
Fabian Zierler <fabian.zierler@a1.net>
2025-03-14 19:02:42 +0000

    further simplify main python script

--- setup_run.py
+++ setup_run.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # coding: utf-8
-
 import llranalysis.inputfiles as ifiles
 import pandas as pd 
 import os.path as op
@@ -11,28 +10,22 @@ infofile   = "./input/local_tests.csv"
 bash_files = ["sp4_llr_start.sh","sp4_llr_start_cont.sh","sp4_llr_cont.sh","sp4_llr_fxa.sh"]
 
 ## create a suitable name for the run:
-input_data = pd.read_csv(infofile)
+def get_run_name(input_data):
+    Lt  = input_data["Lt"].values[0]
+    Ls  = input_data["Ls"].values[0]
+    Rep = input_data["n_replicas"].values[0]
+    return f"LLR_{Lt}x{Ls}_{Rep}"
+
+input_data     = pd.read_csv(infofile)
 template_dir   = input_data["machine"].values[0]
 cores_per_node = input_data["cores_per_node"].values[0]
-Lt  = input_data["Lt"].values[0]
-Ls  = input_data["Ls"].values[0]
-Rep = input_data["n_replicas"].values[0]
-run_name = "LLR_"+str(Lt)+"x"+str(Ls)+"_"+str(Rep)
-folder = op.join(outdir,run_name)
+run_name       = get_run_name(input_data)
+folder         = op.join(outdir,run_name)
 
 ifiles.predat_from_csv(folder,infofile)
 ifiles.copy_identical_files(folder,input_dir)
 ifiles.input_files_from_csv(op.join(input_dir,"base","input_file"),     op.join(folder,"base","input_file"),infofile)
 ifiles.input_files_from_csv(op.join(input_dir,"base","input_file_rep"), op.join(folder,"base","input_file_rep"),infofile)
-
-if template_dir == "local":
-    infile  = op.join(input_dir,"setup_llr_repeat_local.sh")
-else:
-    infile  = op.join(input_dir,"setup_llr_repeat.sh")
-outfile = op.join(folder,"setup_llr_repeat.sh")
-ifiles.input_files_from_csv(infile,outfile,infofile)
-
+ifiles.input_files_from_csv(op.join(input_dir,template_dir,"setup_llr_repeat.sh"),op.join(folder,"setup_llr_repeat.sh"),infofile)
 for name in bash_files:
-    infile  = op.join(input_dir,template_dir,name)
-    outfile = op.join(folder,name)
-    ifiles.edit_batch_files(infile,outfile,infofile,cores_per_node)
+    ifiles.edit_batch_files(op.join(input_dir,template_dir,name),op.join(folder,name),infofile,cores_per_node)
