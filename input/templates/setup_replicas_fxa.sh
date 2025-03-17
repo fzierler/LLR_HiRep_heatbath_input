587f50d0eb2961f3b34b5f54a7c0cb7d5435bdab
Fabian Zierler <fabian.zierler@a1.net>
2025-03-17 19:56:21 +0000

    formatting

--- input/templates/setup_replicas_fxa.sh
+++ input/templates/setup_replicas_fxa.sh
@@ -38,16 +38,17 @@ for (( i=0; i<$r; i+=1 )); do
 
     cp Rep_${i}/input_file Rep_${i}/input_file_${M}
     cp Rep_${i}/rand_state Rep_${i}/rand_state_${M}
-    sed -i "/rlx_seed/c\rlx_seed = ${RANDOM}" Rep_${i}/input_file
+    
+    sed -i "/rlx_seed/c\rlx_seed = ${RANDOM}"         Rep_${i}/input_file
     sed -i "/gauge start/c\gauge start = ${New_File}" Rep_${i}/input_file
-    sed -i "/llr:S0/c\llr:S0 = $E" Rep_${i}/input_file
-    sed -i "/llr:dS/c\llr:dS = ${de}" Rep_${i}/input_file
-    sed -i "/llr:starta/c\llr:starta = ${A}" Rep_${i}/input_file
+    sed -i "/llr:S0/c\llr:S0 = $E"                    Rep_${i}/input_file
+    sed -i "/llr:dS/c\llr:dS = ${de}"                 Rep_${i}/input_file
+    sed -i "/llr:starta/c\llr:starta = ${A}"          Rep_${i}/input_file
     # Everything below does never change
-    sed -i "/rlx_start/c\rlx_start = rand_state" Rep_${i}/input_file
-    sed -i "/llr:nfxa/c\llr:nfxa = 50" Rep_${i}/input_file
-    sed -i "/last conf/c\last conf = +0" Rep_${i}/input_file
-    sed -i "/llr:N_nr/c\llr:N_nr = 0" Rep_${i}/input_file
+    sed -i "/rlx_start/c\rlx_start = rand_state"  Rep_${i}/input_file
+    sed -i "/llr:nfxa/c\llr:nfxa = 50"            Rep_${i}/input_file
+    sed -i "/last conf/c\last conf = +0"          Rep_${i}/input_file
+    sed -i "/llr:N_nr/c\llr:N_nr = 0"             Rep_${i}/input_file
     sed -i "/llr:sfreq_fxa/c\llr:sfreq_fxa = 100" Rep_${i}/input_file
 
 done
