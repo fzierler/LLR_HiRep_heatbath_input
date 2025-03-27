2583903e4cb35cf0101e516a3182c19d599e2447
Fabian Zierler <fabian.zierler@a1.net>
2025-03-27 15:18:50 +0000

    compare inital guesses at different Nt

--- plot_start.py
+++ plot_start.py
@@ -12,24 +12,34 @@ def read_input(init_file):
     umin = np.min(plaq)
     umax = np.max(plaq)
     # Add some data that needs to be interpolate
-    DeltaU = (umax-umin)/5
+    DeltaU = (umax-umin)/20
     uks  = np.linspace(umin-DeltaU,umax+DeltaU, 100)
     return beta, plaq, uks 
 
-init_file_std = "input/data/Nt4_std_beta_vs_S0.txt"
-init_file_llr = "input/data/4x40_25repeats_128replicas.txt"
+init_file_std_Nt4 = "input/data/Nt4_std_beta_vs_S0.txt"
+init_file_llr_Nt4 = ["input/data/4x40_25repeats_128replicas.txt"]
+
+init_file_std_Nt5 = "input/data/Nt5_std_beta_vs_S0.txt"
+init_file_llr_Nt5 = ["input/data/5x72_10repeats_95replicas.txt"]
+
+init_file_std_Nt6 = "input/data/Nt6_std_beta_vs_S0.txt"
+init_file_llr_Nt6 = ["input/data/6x72_25repeats_48replicas.txt"]
+
+init_file_std = init_file_std_Nt6
+init_file_llr = init_file_llr_Nt6
+
+# Plot initial guess based on existing llr results with spline interpolation
+for f in init_file_llr:
+    beta, plaq, uks = read_input(f)
+    aks_int = llr.interpolate_initial_an(plaq,beta,uks)
+    plt.scatter(plaq,beta,label="llr data")
+    plt.plot(uks,aks_int,label="interpolate llr")
 
 # Plot initial guess based on importance sampling data with a fit
 beta, plaq, uks = read_input(init_file_std)
 aks_fit = llr.fit_initial_an(plaq,beta,uks)
 plt.scatter(plaq,beta,label="importance sampling data")
 plt.plot(uks,aks_fit,label="fit importance sampling")
-
-# Plot initial guess based on existing llr results with spline interpolation
-beta, plaq, uks = read_input(init_file_llr)
-aks_int = llr.interpolate_initial_an(plaq,beta,uks)
-plt.scatter(plaq,beta,label="llr data")
-plt.plot(uks,aks_int,label="interpolate llr")
 
 plt.title("Initial guess for $a_n$")
 plt.legend()
