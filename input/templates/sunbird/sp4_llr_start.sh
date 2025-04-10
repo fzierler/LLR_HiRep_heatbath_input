9c6f46cd8ac71744f17ce373da1909b157d6e273
Fabian Zierler <fabian.zierler@a1.net>
2025-04-10 10:44:49 +0100

    add sunbird templates

---
+++ input/templates/sunbird/sp4_llr_start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+#SBATCH --exclusive
+#SBATCH -p compute
+#SBATCH -t 3-00:00:00          
+#SBATCH --nodes=1
+#SBATCH --ntasks=40
+#SBATCH --ntasks-per-node=40
+
+#SBATCH -o %x.out
+#SBATCH -e %x.err
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=fabian.zierler@swansea.ac.uk
+
+### 
+srun -n 76 ../../llr_hb -i input_file_start
---
+++ input/templates/sunbird/sp4_llr_start_cont.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --exclusive
+#SBATCH -p compute
+#SBATCH -t 3-00:00:00          
+#SBATCH --nodes=1
+#SBATCH --ntasks=40
+#SBATCH --ntasks-per-node=40
+
+#SBATCH -o %x.out
+#SBATCH -e %x.err
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=fabian.zierler@swansea.ac.uk
+
+###
+bash ../update_replicas.sh -r 19 -i input_file_start_cont
+srun -n 76 ../../llr_hb -i input_file_start_cont
