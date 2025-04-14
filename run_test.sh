5adaab42aba7cca3a44ccec7e16b6a3b974a6479
Fabian Zierler <fabian.zierler@a1.net>
2025-04-14 16:06:42 +0100

    adapt local tests, compare different HiRepLLR commits

--- run_test.sh
+++ run_test.sh
@@ -1,7 +1,10 @@
 mkdir -p output
+
 cd output
 git clone https://github.com/fzierler/Hirep_LLR_SP
 cd Hirep_LLR_SP/LLR_HB/
+git checkout llr_heatbath_new_spn
+
 make 
 cp ./llr_hb ../../
 cd ../../../
@@ -16,6 +19,22 @@ cd output/LLR_4x4_8/
 rm -rf ../0_reference/ 
 bash setup_llr_repeat.sh
 mv 0/ ../0_reference/
+cd -
+
+cd output
+git clone https://github.com/fzierler/Hirep_LLR_SP
+cd Hirep_LLR_SP/LLR_HB/
+git checkout cb25283d87269a684a25f6049290595cb0ba2d11
+
+make 
+cp ./llr_hb ../../
+cd ../../../
+
+cd output
+rm -rf ./LLR_4x4_8/
+cd -
+python setup_run.py
+cd output/LLR_4x4_8/
 
 # Now repeat the run again, to test whether it is fully reproducible
 rm -rf ../0_new/
@@ -23,7 +42,7 @@ bash setup_llr_repeat.sh
 mv 0/ ../0_new/
 
 # We need to remove the output files beore comparing since they differ in the timings of the operations
-find .. -name out_0 | xargs rm
+#find .. -name out_0 | xargs rm
 
-diff -rqwB ../0_new/ ../0_reference/
+#diff -rqwB ../0_new/ ../0_reference/
 meld ../0_new/ ../0_reference/
