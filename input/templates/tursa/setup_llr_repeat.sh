06fd86ff6164dcdc3f92fdf8d46af4afc350607b
Fabian Zierler <fabian.zierler@a1.net>
2025-04-15 13:00:33 +0100

    create templates for tursa

---
+++ input/templates/tursa/setup_llr_repeat.sh
@@ -0,0 +1,19 @@
+n_RM=10
+n_NR=10
+run_name="sp4_5x80_95"
+
+bname=$(basename "$PWD")
+for i in {0..0}; do
+    # create repeat dircetory
+    cp base $i/ -r
+    cd $i
+
+    # submit all jobs
+    id=$(sbatch --parsable -J "${bname}_repeat${i}_therm" ../sp4_llr_therm.sh)
+    id=$(sbatch --array=1-${n_NR}%1 --parsable -J "${bname}_repeat${i}_newton_raphson" --dependency=afterok:$id ../sp4_llr_newton_raphson.sh)
+    id=$(sbatch --array=1-${n_RM}%1 --parsable -J "${bname}_repeat${i}_robbins_monro"  --dependency=afterok:$id ../sp4_llr_robbins_monro.sh)
+    sbatch --dependency=afterok:$id -J "${bname}_repeat${i}_fxa" ../sp4_llr_fxa.sh
+
+    # move on to next repeat
+    cd ..
+done
---
+++ input/templates/tursa/sp4_llr_robbins_monro.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+#SBATCH --account=dp208
+#SBATCH --nodes=1
+#SBATCH --time=2-00:00:00
+#SBATCH --ntasks=64
+#SBATCH --ntasks-per-node=64
+#SBATCH --qos=standard
+#SBATCH -o %x.out
+#SBATCH -e %x.err
+
+# Use modules to setup the runtime environment
+module load gcc/9.3.0 openmpi/4.1.5
+###
+bash ../update_replicas.sh -r 19 -i input_file_robbins_monro 
+srun -n 76 ../../llr_hb -i input_file_robbins_monro
