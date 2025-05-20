c78ecf1ad243a4ffa87524bb4e3bdd3b8921082d
Fabian Zierler <fabian.zierler@a1.net>
2025-05-20 10:42:14 +0100

    use more generic template files for leonardo

--- input/templates/leonardo/sp4_llr_newton_raphson.sh
+++ input/templates/leonardo/sp4_llr_newton_raphson.sh
@@ -1,21 +1,21 @@
 #!/bin/bash 
 
-#SBATCH --account=EUHPC_B22_046_0
-#SBATCH --partition=dcgp_usr_prod
-#SBATCH --time=0-24:00:00
+#SBATCH --account=
+#SBATCH --partition=
+#SBATCH --time=
 
-#SBATCH --nodes=1
-#SBATCH --ntasks=128
-#SBATCH --ntasks-per-node=128
+#SBATCH --nodes=
+#SBATCH --ntasks=
+#SBATCH --ntasks-per-node=
 
 #SBATCH -o %x.out
 #SBATCH -e %x.err
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --mail-user=fabian.zierler@swansea.ac.uk
+#SBATCH --mail-user=
 
 # Use modules to setup the runtime environment
 module purge
-module load cuda/12.2 nvhpc/23.11 fftw/3.3.10--openmpi--4.1.6--gcc--12.2.0 hdf5
+module load
 
 ###
 bash ../update_replicas.sh -r 19 -i input_file_newton_raphson
