9af4d806b2114df804b746306a344617eef1a0fe
Fabian Zierler <fabian.zierler@a1.net>
2025-05-20 10:04:26 +0100

    use one main file and pass infofile

--- setup_run.py
+++ setup_run.py
@@ -5,18 +5,7 @@ import pandas as pd
 import os.path as op
 import os
 from shutil import copyfile
-
-outdir     = "./output/"
-input_dir  = "./input/templates/"
-infofile   = "./input/local_tests.csv"
-infofile   = "./input/dial3.csv"
-infofile   = "./input/sunbird.csv"
-infofile   = "./input/tursa.csv"
-infofile   = "./input/leonardo.csv"
-
-bash_files  = ["sp4_llr_therm.sh","sp4_llr_newton_raphson.sh","sp4_llr_robbins_monro.sh","sp4_llr_fxa.sh"]
-input_files = ["input_file_therm", "input_file_newton_raphson", "input_file_robbins_monro", "input_file_fxa"]
-setup_files = ["list_configs.sh","update_replicas.sh"]
+from argparse import ArgumentParser, FileType
 
 ## create a suitable name for the run:
 def get_run_name(input_data):
@@ -25,38 +14,55 @@ def get_run_name(input_data):
     Rep = input_data["n_replicas"].values[0]
     return f"LLR_{Lt}x{Ls}_{Rep}"
 
-input_data     = pd.read_csv(infofile)
-template_dir   = input_data["machine"].values[0]
-cores_per_node = input_data["cores_per_node"].values[0]
-run_name       = get_run_name(input_data)
-folder         = op.join(outdir,run_name)
+def main(infofile):
+    outdir     = "./output/"
+    input_dir  = "./input/templates/"
+    bash_files  = ["sp4_llr_therm.sh","sp4_llr_newton_raphson.sh","sp4_llr_robbins_monro.sh","sp4_llr_fxa.sh"]
+    input_files = ["input_file_therm", "input_file_newton_raphson", "input_file_robbins_monro", "input_file_fxa"]
+    setup_files = ["list_configs.sh","update_replicas.sh"]
 
-os.makedirs(os.path.join(folder,"base"), exist_ok=True)
-newinfofile = os.path.join(folder,"base","info.csv")
-copyfile(infofile,newinfofile)
+    input_data     = pd.read_csv(infofile)
+    template_dir   = input_data["machine"].values[0]
+    cores_per_node = input_data["cores_per_node"].values[0]
+    run_name       = get_run_name(input_data)
+    folder         = op.join(outdir,run_name)
 
-Eks, aks, dE, nreplicas = ifiles.initial_an(newinfofile)
-ifiles.setup_bash_files(op.join(input_dir,template_dir,"setup_llr_repeat.sh"),op.join(folder,"setup_llr_repeat.sh"),newinfofile)
+    os.makedirs(os.path.join(folder,"base"), exist_ok=True)
+    newinfofile = os.path.join(folder,"base","info.csv")
+    copyfile(infofile,newinfofile)
 
-for f in setup_files:
-    src = os.path.join(input_dir,f)
-    dst = os.path.join(folder ,f)
-    copyfile(src,dst)
+    Eks, aks, dE, nreplicas = ifiles.initial_an(newinfofile)
+    ifiles.setup_bash_files(op.join(input_dir,template_dir,"setup_llr_repeat.sh"),op.join(folder,"setup_llr_repeat.sh"),newinfofile)
 
-for infile in input_files:
-    ifiles.setup_input_files(op.join(input_dir,"base","input_file"),     op.join(folder,"base",infile)         ,newinfofile)
+    for f in setup_files:
+        src = os.path.join(input_dir,f)
+        dst = os.path.join(folder ,f)
+        copyfile(src,dst)
+
+    for infile in input_files:
+        ifiles.setup_input_files(op.join(input_dir,"base","input_file"),op.join(folder,"base",infile),newinfofile)
+        for i in range(nreplicas):
+            replica_dir = os.path.join(folder,"base",f"Rep_{i}")
+            in_replica = op.join(input_dir,"base","input_file_rep")
+            out_replica  = op.join(folder,"base",f"Rep_{i}",infile)
+            os.makedirs(replica_dir,exist_ok=True)
+            ifiles.setup_input_files(in_replica, out_replica, newinfofile)
+            ifiles.setup_initial_an_inplace(out_replica, min(Eks), max(Eks), Eks[i], dE, aks[i])
+
     for i in range(nreplicas):
-        replica_dir = os.path.join(folder,"base",f"Rep_{i}")
-        in_replica = op.join(input_dir,"base","input_file_rep")
-        out_replica  = op.join(folder,"base",f"Rep_{i}",infile)
-        os.makedirs(replica_dir,exist_ok=True)
-        ifiles.setup_input_files(in_replica, out_replica, newinfofile)
-        ifiles.setup_initial_an_inplace(out_replica, min(Eks), max(Eks), Eks[i], dE, aks[i])
+        ifiles.setup_fxa_input_inplace(op.join(folder,"base",f"Rep_{i}","input_file_fxa"))
+        ifiles.setup_nr_input_inplace(op.join(folder,"base",f"Rep_{i}","input_file_newton_raphson"),infofile)
+        ifiles.setup_rm_input_inplace(op.join(folder,"base",f"Rep_{i}","input_file_robbins_monro"),infofile)
 
-for i in range(nreplicas):
-    ifiles.setup_fxa_input_inplace(op.join(folder,"base",f"Rep_{i}","input_file_fxa"))
-    ifiles.setup_nr_input_inplace(op.join(folder,"base",f"Rep_{i}","input_file_newton_raphson"),infofile)
-    ifiles.setup_rm_input_inplace(op.join(folder,"base",f"Rep_{i}","input_file_robbins_monro"),infofile)
+    for name in bash_files:
+        ifiles.setup_batch_files(op.join(input_dir,template_dir,name),op.join(folder,name),newinfofile,cores_per_node)
 
-for name in bash_files:
-    ifiles.setup_batch_files(op.join(input_dir,template_dir,name),op.join(folder,name),newinfofile,cores_per_node)
+def get_args():
+    parser = ArgumentParser(description="Set up structure for LLR heatbath runs with HiRep")
+    parser.add_argument("--infofile", help="The csv file with input parameters to read")
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = get_args()
+    infofile = args.infofile
+    main(infofile)
