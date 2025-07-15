8287ee6fe683b5c9a0407ac52a58bb77340f14ec
Fabian Zierler <fabian.zierler@a1.net>
2025-07-15 20:43:29 +0100

    more DiRAC18 scaling tests

--- machine_defaults.sh
+++ machine_defaults.sh
@@ -1,1 +1,3 @@
-python3 main.py --input_params_csv input/csd3.csv --machine csd3 --partition icelake --account DIRAC-DP208-CPU --qos dirac-cpu1 --email fabian.zierler@swansea.ac.uk --path_llr_exec "../.." --modules "openmpi/gcc/9.3/4.0.4 gcc/9"
+python3 main.py --input_params_csv input/csd3.csv --machine csd3 --partition icelake --account DIRAC-DP208-CPU --qos dirac-cpu1 --modules "openmpi/gcc/9.3/4.0.4 gcc/9" --email fabian.zierler@swansea.ac.uk --path_llr_exec "../.."
+python3 main.py --input_params_csv input/tursa.csv --machine tursa --partition cpu --account dp208 --qos standard --modules "gcc/9.3.0 openmpi/4.1.5" --email fabian.zierler@swansea.ac.uk --path_llr_exec "../.."
+python3 main.py --input_params_csv input/dial3.csv --machine dial --partition slurm --account dp208 --qos high --modules "gcc/10.3.0 openmpi/4.0.5" --email fabian.zierler@swansea.ac.uk --path_llr_exec "../.."
