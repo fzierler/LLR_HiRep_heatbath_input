c39f243129c294a347b6395bc4244164af5527ed
Fabian Zierler <fabian.zierler@a1.net>
2026-02-15 19:38:44 +0100

    update slurm templates

--- input/templates/generic/sp4_llr_newton_raphson.sh
+++ input/templates/generic/sp4_llr_newton_raphson.sh
@@ -8,6 +8,8 @@
 #SBATCH --nodes=
 #SBATCH --ntasks=
 #SBATCH --ntasks-per-node=
+#SBATCH --cpus-per-task=1
+#SBATCH --threads-per-core=1
 
 #SBATCH -o %x.out
 #SBATCH -e %x.err
@@ -15,8 +17,9 @@
 #SBATCH --mail-user=
 
 # Use modules to setup the runtime environment
-#module purge
 module load
+
+export SRUN_CPUS_PER_TASK=${SLURM_CPUS_PER_TASK}
 
 ###
 bash ../update_replicas.sh -r 19 -i input_file_newton_raphson
