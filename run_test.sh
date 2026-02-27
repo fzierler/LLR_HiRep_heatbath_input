616bbc47b64dca05a0c18cea4290e25fe421b3ec
Fabian Zierler <fabian.zierler@a1.net>
2026-02-27 17:57:39 +0100

    revive local testing

--- run_test.sh
+++ run_test.sh
@@ -10,39 +10,10 @@ cp ./llr_hb ../../
 cd ../../../
 
 cd output
-rm -rf ./LLR_4x4_8/
+rm -rf ./LLR_sp4_4x4_8_Run_1/
 cd -
-python setup_run.py
-cd output/LLR_4x4_8/
 
-# now we have a test run in output/LLR_4x4_8/0/
-rm -rf ../0_reference/ 
+python3 main.py --input_params_csv input/local_tests.csv --machine local --mpi_runner mpirun --path_llr_exec "../.."
+cd output/LLR_sp4_4x4_8_Run_1/
+
 bash setup_llr_repeat.sh
-mv 0/ ../0_reference/
-cd -
-
-cd output
-git clone https://github.com/fzierler/Hirep_LLR_SP
-cd Hirep_LLR_SP/LLR_HB/
-git checkout cb25283d87269a684a25f6049290595cb0ba2d11
-
-make 
-cp ./llr_hb ../../
-cd ../../../
-
-cd output
-rm -rf ./LLR_4x4_8/
-cd -
-python setup_run.py
-cd output/LLR_4x4_8/
-
-# Now repeat the run again, to test whether it is fully reproducible
-rm -rf ../0_new/
-bash setup_llr_repeat.sh
-mv 0/ ../0_new/
-
-# We need to remove the output files beore comparing since they differ in the timings of the operations
-#find .. -name out_0 | xargs rm
-
-#diff -rqwB ../0_new/ ../0_reference/
-meld ../0_new/ ../0_reference/
